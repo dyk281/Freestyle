f51088d38528807fa7f7572cc902ad4dc54811a1
dyk281 <dyk281@stern.nyu.edu>
2017-08-05 14:15:10 -0400

    add dict

--- APP/app.py
+++ APP/app.py
@@ -1,14 +1,20 @@
 import csv
 
 stock = [] #1. create empty list called product
-
-csv_file_path = "data/products.csv"
-
+csv_file_path = "data/stock.csv"
 # READ PRODUCTS CSV
-
-#we will loop through each product
 with open(csv_file_path, "r") as csv_file:
-    reader = csv.DictReader(csv_file) #3. using csv.DictReader function, each row will be dictionary like
-    for row in reader: #2. when reading the file, iterating through each "row"
-    #each row corresponds to a product
-        products.append(row) #4 append each "row" to "products"
+    reader = csv.DictReader(csv_file) #using csv.DictReader function, each row will be dictionary like
+    for row in reader:
+        stock.append(row) #append each "row" to "products"
+
+forecast = [] #1. create empty list called product
+csv_file_path = "data/forecast.csv"
+
+with open(csv_file_path, "r") as csv_file:
+    reader = csv.DictReader(csv_file) #using csv.DictReader function, each row will be dictionary like
+    for row in reader:
+        forecast.append(row)
+
+print (stock)
+print (forecast)
